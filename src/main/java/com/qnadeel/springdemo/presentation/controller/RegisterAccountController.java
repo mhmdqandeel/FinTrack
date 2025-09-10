88d41884880b263b650e879b58418109e2a6a357
qandeel <mohaqan2004@gmail.com>
2025-09-11 02:24:47 +0300

    add doc for RegisterAccount endpoint

--- src/main/java/com/qnadeel/springdemo/presentation/controller/RegisterAccountController.java
+++ src/main/java/com/qnadeel/springdemo/presentation/controller/RegisterAccountController.java
@@ -5,6 +5,11 @@ import com.qnadeel.springdemo.application.RegisterAccountUseCase;
 import com.qnadeel.springdemo.core.entities.user.entity.User;
 import com.qnadeel.springdemo.presentation.dto.RegisterAccountRequest;
 import com.qnadeel.springdemo.presentation.dto.RegisterAccountResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,6 +25,13 @@ public class RegisterAccountController {
 
     private final RegisterAccountUseCase registerAccountUseCase;
 
+    @Operation(summary = "Register a new user account", description = "Creates a new user account with name, email, and password")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Registration successful.Please check your email to verify your account",
+                    content = @Content(schema = @Schema)),
+            @ApiResponse(responseCode = "400", description = "Invalid request payload",
+                    content = @Content),
+    })
     @PostMapping("/register")
     public ResponseEntity<RegisterAccountResponse> register(@RequestBody RegisterAccountRequest request){
         User user = registerAccountUseCase.execute(new RegisterAccountCommand(
