f60ec52266b652eb221e5d2ef91faab83b89dd34
qandeel <mohaqan2004@gmail.com>
2025-09-11 01:10:47 +0300

    make RegisterAccountController work

---
+++ src/main/java/com/qnadeel/springdemo/presentation/controller/RegisterAccountController.java
@@ -0,0 +1,33 @@
+package com.qnadeel.springdemo.presentation.controller;
+
+import com.qnadeel.springdemo.application.RegisterAccountCommand;
+import com.qnadeel.springdemo.application.RegisterAccountUseCase;
+import com.qnadeel.springdemo.core.entities.user.entity.User;
+import com.qnadeel.springdemo.presentation.dto.RegisterAccountRequest;
+import com.qnadeel.springdemo.presentation.dto.RegisterAccountResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@AllArgsConstructor
+public class RegisterAccountController {
+
+    private final RegisterAccountUseCase registerAccountUseCase;
+
+    @PostMapping("/register")
+    public ResponseEntity<RegisterAccountResponse> register(@RequestBody RegisterAccountRequest request){
+        User user = registerAccountUseCase.execute(new RegisterAccountCommand(
+                request.name(),
+                request.email(),
+                request.password()
+        ));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(new RegisterAccountResponse(user));
+    }
+}
--- src/main/java/com/qnadeel/springdemo/presentation/dto/RegisterAccountRequest.java
+++ src/main/java/com/qnadeel/springdemo/presentation/dto/RegisterAccountRequest.java
@@ -1,4 +1,4 @@
-package com.qnadeel.springdemo.presentation;
+package com.qnadeel.springdemo.presentation.dto;
 
 import jakarta.validation.constraints.Email;
 
