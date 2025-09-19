b070d1dfc71f987532f6b7ce51e152441eb5cba0
qandeel <mohaqan2004@gmail.com>
2025-09-19 21:36:06 +0300

    refact CreateCategoryUseCase

--- src/main/java/com/qnadeel/springdemo/application/category/CreateCategoryUseCase.java
+++ src/main/java/com/qnadeel/springdemo/application/category/CreateCategoryUseCase.java
@@ -2,6 +2,7 @@ package com.qnadeel.springdemo.application.category;
 
 import com.qnadeel.springdemo.application.RegisterAccount.CreateCategoryCommand;
 import com.qnadeel.springdemo.core.entities.category.Category;
+import com.qnadeel.springdemo.core.entities.category.CategoryFactory;
 import com.qnadeel.springdemo.core.entities.category.CategoryRepository;
 import com.qnadeel.springdemo.core.entities.user.UserRepository;
 import com.qnadeel.springdemo.core.entities.user.entity.User;
@@ -14,14 +15,13 @@ public class CreateCategoryUseCase {
 
     private final UserRepository userRepository;
     private final CategoryRepository categoryRepository;
+    private final CategoryFactory categoryFactory;
+
 
     public Category execute(CreateCategoryCommand command) {
         User user = userRepository.getOrThrowByID(command.userId());
 
-        Category category = Category.builder()
-                .name(command.category())
-                .createdBy(user)
-                .build();
+        Category category = categoryFactory.create(command.category(), user);
 
         return categoryRepository.save(category);
     }
